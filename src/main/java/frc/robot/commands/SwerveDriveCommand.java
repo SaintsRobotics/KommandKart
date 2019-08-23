ae294f0b1ad4512e4c5aca2d607b9d88445fb159
Delaynie McMillan <delaynieym@outlook.com>
2019-08-22 20:44:57 -0700

    changed spaces to tabs

--- src/main/java/frc/robot/commands/SwerveDriveCommand.java
+++ src/main/java/frc/robot/commands/SwerveDriveCommand.java
@@ -8,90 +8,90 @@ import frc.robot.subsystems.SwerveSubsystem;
 
 public class SwerveDriveCommand extends Command {
 
-    private SwerveSubsystem m_subsystem;
-    private DoubleSupplier m_transX;
-    private DoubleSupplier m_transY;
-    private DoubleSupplier m_rotation;
-    private BooleanSupplier m_absolute;
-    private boolean m_dynamicGain;
+	private SwerveSubsystem m_subsystem;
+	private DoubleSupplier m_transX;
+	private DoubleSupplier m_transY;
+	private DoubleSupplier m_rotation;
+	private BooleanSupplier m_absolute;
+	private boolean m_dynamicGain;
 
-    /**
-     * 
-     * @param subsystem     swerve drive subsystem
-     * @param transX        joystick access method for moving the robot right and
-     *                      left
-     * @param transY        joystick access method for moving the robot back and
-     *                      forth
-     * @param rotation      joystick access method for rotating the robot
-     * @param absoluteDrive whether or not the robot will drive relative to the
-     *                      direction it's facing or the direction it started in.
-     *                      Invert this to have absolute drive as default.
-     */
-    public SwerveDriveCommand(SwerveSubsystem subsystem, DoubleSupplier transX, DoubleSupplier transY,
-            DoubleSupplier rotation, BooleanSupplier absoluteDrive) {
-        this.m_subsystem = subsystem;
-        requires(this.m_subsystem);
+	/**
+	 * 
+	 * @param subsystem     swerve drive subsystem
+	 * @param transX        joystick access method for moving the robot right and
+	 *                      left
+	 * @param transY        joystick access method for moving the robot back and
+	 *                      forth
+	 * @param rotation      joystick access method for rotating the robot
+	 * @param absoluteDrive whether or not the robot will drive relative to the
+	 *                      direction it's facing or the direction it started in.
+	 *                      Invert this to have absolute drive as default.
+	 */
+	public SwerveDriveCommand(SwerveSubsystem subsystem, DoubleSupplier transX, DoubleSupplier transY,
+			DoubleSupplier rotation, BooleanSupplier absoluteDrive) {
+		this.m_subsystem = subsystem;
+		requires(this.m_subsystem);
 
-        this.m_transX = transX;
-        this.m_transY = transY;
-        this.m_rotation = rotation;
-        this.m_absolute = absoluteDrive;
-        this.m_dynamicGain = true;
-    }
+		this.m_transX = transX;
+		this.m_transY = transY;
+		this.m_rotation = rotation;
+		this.m_absolute = absoluteDrive;
+		this.m_dynamicGain = true;
+	}
 
-    /**
-     * 
-     * @param subsystem     swerve drive subsystem
-     * @param transX        joystick access method for moving the robot right and
-     *                      left
-     * @param transY        joystick access method for moving the robot back and
-     *                      forth
-     * @param rotation      joystick access method for rotating the robot
-     * @param absoluteDrive whether or not the robot will drive relative to the
-     *                      direction it's facing or the direction it started in.
-     *                      invert this to have absolute drive as default
-     * @param dynamicGain   drive with dynamic translation/rotation gain or static
-     *                      translation/rotation gain. defaults to true
-     */
-    public SwerveDriveCommand(SwerveSubsystem subsystem, DoubleSupplier transX, DoubleSupplier transY,
-            DoubleSupplier rotation, BooleanSupplier absoluteDrive, boolean dynamicGain) {
-        this(subsystem, transX, transY, rotation, absoluteDrive);
-        this.m_dynamicGain = dynamicGain;
-    }
+	/**
+	 * 
+	 * @param subsystem     swerve drive subsystem
+	 * @param transX        joystick access method for moving the robot right and
+	 *                      left
+	 * @param transY        joystick access method for moving the robot back and
+	 *                      forth
+	 * @param rotation      joystick access method for rotating the robot
+	 * @param absoluteDrive whether or not the robot will drive relative to the
+	 *                      direction it's facing or the direction it started in.
+	 *                      invert this to have absolute drive as default
+	 * @param dynamicGain   drive with dynamic translation/rotation gain or static
+	 *                      translation/rotation gain. defaults to true
+	 */
+	public SwerveDriveCommand(SwerveSubsystem subsystem, DoubleSupplier transX, DoubleSupplier transY,
+			DoubleSupplier rotation, BooleanSupplier absoluteDrive, boolean dynamicGain) {
+		this(subsystem, transX, transY, rotation, absoluteDrive);
+		this.m_dynamicGain = dynamicGain;
+	}
 
-    // Called just before this Command runs the first time
-    @Override
-    protected void initialize() {
-    }
+	// Called just before this Command runs the first time
+	@Override
+	protected void initialize() {
+	}
 
-    // Called repeatedly when this Command is scheduled to run
-    @Override
-    protected void execute() {
-        if (m_dynamicGain) {
-            m_subsystem.dynamicGainDrive(this.m_transX.getAsDouble(), this.m_transY.getAsDouble(),
-                    this.m_rotation.getAsDouble());
-        } else {
-            m_subsystem.staticGainDrive(this.m_transX.getAsDouble(), this.m_transY.getAsDouble(),
-                    this.m_rotation.getAsDouble());
-        }
+	// Called repeatedly when this Command is scheduled to run
+	@Override
+	protected void execute() {
+		if (m_dynamicGain) {
+			m_subsystem.dynamicGainDrive(this.m_transX.getAsDouble(), this.m_transY.getAsDouble(),
+					this.m_rotation.getAsDouble());
+		} else {
+			m_subsystem.staticGainDrive(this.m_transX.getAsDouble(), this.m_transY.getAsDouble(),
+					this.m_rotation.getAsDouble());
+		}
 
-    }
+	}
 
-    // Make this return true when this Command no longer needs to run execute()
-    @Override
-    protected boolean isFinished() {
-        return false;
-    }
+	// Make this return true when this Command no longer needs to run execute()
+	@Override
+	protected boolean isFinished() {
+		return false;
+	}
 
-    // Called once after isFinished returns true
-    @Override
-    protected void end() {
-    }
+	// Called once after isFinished returns true
+	@Override
+	protected void end() {
+	}
 
-    // Called when another command which requires one or more of the same
-    // subsystems is scheduled to run
-    @Override
-    protected void interrupted() {
-    }
+	// Called when another command which requires one or more of the same
+	// subsystems is scheduled to run
+	@Override
+	protected void interrupted() {
+	}
 
 }
--- src/main/java/frc/robot/subsystems/SwerveWheel.java
+++ src/main/java/frc/robot/subsystems/SwerveWheel.java
@@ -12,111 +12,111 @@ import frc.robot.util.PidConfig;
  */
 public class SwerveWheel {
 
-    private SpeedController m_driveMotor;
-    private PIDSource m_encoder;
-    private PIDController m_pidController;
+	private SpeedController m_driveMotor;
+	private PIDSource m_encoder;
+	private PIDController m_pidController;
 
-    private double[] m_rotationVector;
-    private double m_radius;
+	private double[] m_rotationVector;
+	private double m_radius;
 
-    /**
-     * @param driveMotor   Motor that drives the wheel.
-     * @param turnMotor    Motor that rotates the wheel. Must be a PIDOutput.
-     * @param encoder      Encoder that tracks the position of the turn motor, in
-     *                     degrees. Must be a PIDSource. An absolute encoder is
-     *                     highly recommended.
-     * @param pidConfig    A {@link frc.robot.util.PidConfig PidConfig} holding the
-     *                     values for setting up a PIDController.
-     * @param radiusVector The x and y coordinates of the wheel relative to the
-     *                     origin.
-     */
-    public SwerveWheel(SpeedController driveMotor, PIDOutput turnMotor, PIDSource encoder, PidConfig pidConfig,
-            double[] radiusVector) {
+	/**
+	 * @param driveMotor   Motor that drives the wheel.
+	 * @param turnMotor    Motor that rotates the wheel. Must be a PIDOutput.
+	 * @param encoder      Encoder that tracks the position of the turn motor, in
+	 *                     degrees. Must be a PIDSource. An absolute encoder is
+	 *                     highly recommended.
+	 * @param pidConfig    A {@link frc.robot.util.PidConfig PidConfig} holding the
+	 *                     values for setting up a PIDController.
+	 * @param radiusVector The x and y coordinates of the wheel relative to the
+	 *                     origin.
+	 */
+	public SwerveWheel(SpeedController driveMotor, PIDOutput turnMotor, PIDSource encoder, PidConfig pidConfig,
+			double[] radiusVector) {
 
-        this.m_driveMotor = driveMotor;
-        this.m_encoder = encoder;
+		this.m_driveMotor = driveMotor;
+		this.m_encoder = encoder;
 
-        this.m_pidController = new PIDController(pidConfig.kP, pidConfig.kI, pidConfig.kD, this.m_encoder,
-                (output) -> turnMotor.pidWrite(output));
-        this.m_pidController.setAbsoluteTolerance(pidConfig.tolerance);
-        this.m_pidController.setOutputRange(-01, 01);
-        this.m_pidController.setInputRange(0, 360);
-        this.m_pidController.setContinuous();
-        this.m_pidController.reset();
-        this.m_pidController.enable();
+		this.m_pidController = new PIDController(pidConfig.kP, pidConfig.kI, pidConfig.kD, this.m_encoder,
+				(output) -> turnMotor.pidWrite(output));
+		this.m_pidController.setAbsoluteTolerance(pidConfig.tolerance);
+		this.m_pidController.setOutputRange(-01, 01);
+		this.m_pidController.setInputRange(0, 360);
+		this.m_pidController.setContinuous();
+		this.m_pidController.reset();
+		this.m_pidController.enable();
 
-        this.m_rotationVector = new double[] { -radiusVector[1], radiusVector[0] }; // perpendicular to the radius line
-                                                                                    // (negative reciprocal)
-        this.m_radius = Math.sqrt(Math.pow(radiusVector[0], 2) + Math.pow(radiusVector[1], 2));
-    }
+		this.m_rotationVector = new double[] { -radiusVector[1], radiusVector[0] }; // perpendicular to the radius line
+																					// (negative reciprocal)
+		this.m_radius = Math.sqrt(Math.pow(radiusVector[0], 2) + Math.pow(radiusVector[1], 2));
+	}
 
-    /**
-     * 
-     * @param targetHead     The direction the wheel is to be facing, in degrees. It
-     *                       calls
-     *                       {@link edu.wpi.first.wpilibj.PIDController#setSetpoint(double)
-     *                       PIDController.setSetpoint(double setpoint)} on the
-     *                       given target.
-     * @param targetVelocity The velocity at which the wheel is to be turning. Input
-     *                       is in the element of [-1, 1].
-     */
-    public void setHeadAndVelocity(double targetHead, double targetVelocity) {
-        /*
-         * smart inversion logic: if turning to the angle 180 degrees away from the set
-         * angle is a shorter turn, go to that angle instead and invert the drive motor
-         * speed
-         */
+	/**
+	 * 
+	 * @param targetHead     The direction the wheel is to be facing, in degrees. It
+	 *                       calls
+	 *                       {@link edu.wpi.first.wpilibj.PIDController#setSetpoint(double)
+	 *                       PIDController.setSetpoint(double setpoint)} on the
+	 *                       given target.
+	 * @param targetVelocity The velocity at which the wheel is to be turning. Input
+	 *                       is in the element of [-1, 1].
+	 */
+	public void setHeadAndVelocity(double targetHead, double targetVelocity) {
+		/*
+		 * smart inversion logic: if turning to the angle 180 degrees away from the set
+		 * angle is a shorter turn, go to that angle instead and invert the drive motor
+		 * speed
+		 */
 
-        double diff = 0.0;
-        double currentHead = this.m_encoder.pidGet();
-        if (Math.abs(targetHead - currentHead) > 180) {
-            diff = 360 - Math.abs(targetHead - currentHead);
-        } else {
-            diff = Math.abs(targetHead - currentHead);
-        }
-        if (diff > 90) {
-            targetHead += 180;
-            targetHead %= 360;
-            targetVelocity = -targetVelocity;
-        }
+		double diff = 0.0;
+		double currentHead = this.m_encoder.pidGet();
+		if (Math.abs(targetHead - currentHead) > 180) {
+			diff = 360 - Math.abs(targetHead - currentHead);
+		} else {
+			diff = Math.abs(targetHead - currentHead);
+		}
+		if (diff > 90) {
+			targetHead += 180;
+			targetHead %= 360;
+			targetVelocity = -targetVelocity;
+		}
 
-        this.setVelocity(targetVelocity);
-        this.m_pidController.setSetpoint(targetHead);
-    }
+		this.setVelocity(targetVelocity);
+		this.m_pidController.setSetpoint(targetHead);
+	}
 
-    /**
-     * Directly sets the given speed controller.
-     * 
-     * @param velocity Input is in the element of [-1, 1].
-     */
-    public void setVelocity(double velocity) {
-        this.m_driveMotor.set(velocity);
-    }
+	/**
+	 * Directly sets the given speed controller.
+	 * 
+	 * @param velocity Input is in the element of [-1, 1].
+	 */
+	public void setVelocity(double velocity) {
+		this.m_driveMotor.set(velocity);
+	}
 
-    /**
-     * Used for calculating the swerve wheel's heading and velocity. It's the
-     * distance from the wheel to the pivot point.
-     */
-    public double getRadius() {
-        return this.m_radius;
-    }
+	/**
+	 * Used for calculating the swerve wheel's heading and velocity. It's the
+	 * distance from the wheel to the pivot point.
+	 */
+	public double getRadius() {
+		return this.m_radius;
+	}
 
-    /**
-     * This value is unique to every wheel. The calculation is based on the pivot
-     * point and wheel location. It is a two-dimensional vector with the length
-     * equal to the radius and a slope perpendicular to the radius. It's also known
-     * as the direction that the wheel will face when the robot is turning in place.
-     */
-    public double[] getRotationVector() {
-        return this.m_rotationVector;
-    }
+	/**
+	 * This value is unique to every wheel. The calculation is based on the pivot
+	 * point and wheel location. It is a two-dimensional vector with the length
+	 * equal to the radius and a slope perpendicular to the radius. It's also known
+	 * as the direction that the wheel will face when the robot is turning in place.
+	 */
+	public double[] getRotationVector() {
+		return this.m_rotationVector;
+	}
 
-    public double getDriveSpeed() {
-        return this.m_driveMotor.get();
-    }
+	public double getDriveSpeed() {
+		return this.m_driveMotor.get();
+	}
 
-    public double getTurnSpeed() {
-        return this.m_pidController.get();
-    }
+	public double getTurnSpeed() {
+		return this.m_pidController.get();
+	}
 
 }
